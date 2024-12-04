ff6138dd4464f894346e923b050531bf33736a5b
JaymieJo <PutraCraft9@gmail.com>
2024-12-04 22:03:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,55 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
 
- # Data baju anak
-baju_anak = [
-    {"Nama": "Baju Anak Motif Hewan", "Harga": 100000, "Gambar": "WEBSITE/baju1"},
-    {"Nama": "Baju Anak Motif Bunga", "Harga": 95000, "Gambar": "images/anak_flower.jpg"},
-    {"Nama": "Baju Anak Motif Mobil", "Harga": 105000, "Gambar": "images/anak_car.jpg"},
-    {"Nama": "Baju Anak Motif Buah", "Harga": 110000, "Gambar": "images/anak_fruit.jpg"}, 
-    {"Nama": "Baju Anak Motif Kartun", "Harga": 120000, "Gambar": "images/anak_cartoon.jpg"},
-    {"Nama": "Baju Anak Motif Polkadot", "Harga": 90000, "Gambar": "images/anak_polkadot.jpg"}, 
-    {"Nama": "Baju Anak Motif Pelangi", "Harga": 115000, "Gambar": "images/anak_rainbow.jpg"},
-    {"Nama": "Baju Anak Motif Bintang", "Harga": 100000, "Gambar": "images/anak_star.jpg"},
-    {"Nama": "Baju Anak Motif Luar Angkasa", "Harga": 92000, "Gambar": "images/anak_space.jpg"},
-]
+# Load the data
+baju_anak = pd.read_csv('data/baju_anak.csv')
 
-# Judul aplikasi
-st.title("Toko Baju Anak")
+# Create a sidebar for filtering the data
+st.sidebar.header('Filter Baju Anak')
+jenis_kelamin = st.sidebar.selectbox('Jenis Kelamin', ['Semua', 'Laki-laki', 'Perempuan'])
+usia = st.sidebar.selectbox('Usia', ['Semua', '0-6 bulan', '6-12 bulan', '1-2 tahun', '2-3 tahun', '3-4 tahun'])
+merek = st.sidebar.selectbox('Merek', ['Semua'] + baju_anak['Merek'].unique().tolist())
+harga_min = st.sidebar.number_input('Harga Minimum', min_value=0, max_value=1000000)
+harga_max = st.sidebar.number_input('Harga Maksimum', min_value=0, max_value=1000000)
 
-# Katalog baju anak
-for baju in baju_anak:
-    col1, col2 = st.columns([1, 3])
+# Filter the data
+if jenis_kelamin != 'Semua':
+    baju_anak = baju_anak[baju_anak['Jenis Kelamin'] == jenis_kelamin]
+if usia != 'Semua':
+    baju_anak = baju_anak[baju_anak['Usia'] == usia]
+if merek != 'Semua':
+    baju_anak = baju_anak[baju_anak['Merek'] == merek]
+if harga_min > 0:
+    baju_anak = baju_anak[baju_anak['Harga'] >= harga_min]
+if harga_max < 1000000:
+    baju_anak = baju_anak[baju_anak['Harga'] <= harga_max]
 
-    with col1:
-        # Tampilkan gambar
-        st.image(baju["Gambar"], use_column_width=True)
+# Display the data
+st.header('Katalog Baju Anak')
+st.write(baju_anak)
 
-    with col2:
-        # Tampilkan nama dan harga baju
-        st.subheader(baju["Nama"])
-        st.write(f"*Harga:* Rp {baju['Harga']:,}")
+# Create a scatter plot of the data
+st.header('Scatter Plot of Baju Anak')
+fig, ax = plt.subplots()
+ax.scatter(baju_anak['Usia'], baju_anak['Harga'])
+ax.set_xlabel('Usia')
+ax.set_ylabel('Harga')
+st.pyplot(fig)
 
-# Keranjang belanja
-keranjang = st.empty()
+# Create a bar chart of the data
+st.header('Bar Chart of Baju Anak')
+fig, ax = plt.subplots()
+ax.bar(baju_anak['Merek'], baju_anak['Harga'])
+ax.set_xlabel('Merek')
+ax.set_ylabel('Harga')
+st.pyplot(fig)
 
-# Tombol tambah ke keranjang
-def add_to_cart(baju):
-    if baju in keranjang.empty():
-        keranjang.empty()
-    
-    if baju not in keranjang:
-        keranjang.write(f"* {baju['Nama']} - Rp {baju['Harga']:,}")
-
-for baju in baju_anak:
-    st.button(f"Tambah ke Keranjang ({baju['Nama']})", on_click=add_to_cart, args=(baju,))
+# Display the images of the baju anak
+st.header('Gambar Baju Anak')
+for i in range(len(baju_anak)):
+    image = Image.open(f'images/{baju_anak["Gambar"][i]}')
+    st.image(image, caption=baju_anak['Nama Produk'][i])
